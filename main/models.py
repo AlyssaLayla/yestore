0c653ad601f94c13b3cd23ef2ee1e851da5a6253
AlyssaLayla <alyssasasti@gmail.com>
2024-09-10 04:01:41 +0700

    Seventh Push: Modified models.py

--- main/models.py
+++ main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, name="nama")
+    name = models.CharField(max_length=255, name="name")
     price = models.IntegerField(name="price")
     quantity = models.IntegerField(name="quantity")
     description = models.TextField(name="description")
-    category = models.CharField(max_length=255, name="name")
+    category = models.CharField(max_length=255, name="category")
