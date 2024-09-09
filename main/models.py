7b330dd93eadf29993965a63918a470b32396028
AlyssaLayla <alyssasasti@gmail.com>
2024-09-10 03:49:59 +0700

    Fifth Push: Added models.py, revise views.py and main.html

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, name="name")
+    name = models.CharField(max_length=255, name="nama")
     price = models.IntegerField(name="price")
+    quantity = models.IntegerField(name="quantity")
     description = models.TextField(name="description")
+    category = models.CharField(max_length=255, name="name")
--- main/views.py
+++ main/views.py
@@ -4,9 +4,14 @@ from .models import Product
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Dimsum Mozarella',
-        'price': 20000,
-        'description': 'Dimsum dengan keju Mozarella meleleh di atasnya wow enak'
+        'nama': 'Alyssa Layla Sasti',
+        'kelas': 'PBP D',
+        'npm': 2306152052,
+        'products': [ 
+            {'name': 'Dimsum Mozarella', 'price': 20000, 'quantity': 5, 'description': 'Dimsum dengan keju Mozarella meleleh di atasnya wow enak', 'category': 'Makanan'},
+            {'name': 'Charger HP', 'price': 100000, 'quantity': 8, 'description': 'Charge HP dijamin Original!', 'category': 'Elektronik'},
+            {'name': 'Hoodie', 'price': 50000, 'quantity': 3, 'description': 'Hoodie Oversize paling keren se-UI', 'category': 'Pakaian'},
+            ],
     }
 
     return render(request, "main.html", context)
