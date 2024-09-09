83af51950d42af05adb5748b59051e57a21bd68b
AlyssaLayla <alyssasasti@gmail.com>
2024-09-09 22:31:21 +0700

    Third push: Add allowed host

--- yestore/settings.py
+++ yestore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a&_#r3@k5!@b+#jj784^a@6vse!w%p18cfw@^wjvt^q+0tbdd*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
