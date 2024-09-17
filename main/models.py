4dc493cd5fb6e947c5b36865f120af30ca9128fd
AlyssaLayla <alyssasasti@gmail.com>
2024-09-17 18:09:59 +0700

    Tugas 3 PBP

--- main/models.py
+++ main/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid 
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, name="name")
     price = models.IntegerField(name="price")
-    quantity = models.IntegerField(name="quantity")
+    quantity = models.IntegerField(name="quantity", default=0)
     description = models.TextField(name="description")
-    category = models.CharField(max_length=255, name="category")
+    category = models.CharField(max_length=255, name="category", default="Uncategorized")
+
