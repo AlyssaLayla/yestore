759b4d4bbfcffb1721886576031a41279f723415
AlyssaLayla <alyssasasti@gmail.com>
2024-11-20 05:24:17 +0700

    Tugas 9

--- yestore/settings.py
+++ yestore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a&_#r3@k5!@b+#jj784^a@6vse!w%p18cfw@^wjvt^q+0tbdd*
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alyssa-layla-yestore.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alyssa-layla-yestore.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'yestore.urls'
@@ -132,3 +135,11 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://alyssa-layla-yestore.pbp.cs.ui.ac.id", "https://alyssa-layla-yestore.pbp.cs.ui.ac.id"]
+
+...
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
